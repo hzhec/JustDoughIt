35db7042643b4e28f018d6e1b2a77810fedd29c0
hzhec93 <chzhee@gmail.com>
2023-08-12 00:52:14 +0800

    Updated route path

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -19,11 +19,11 @@ const Main = () => {
 				<Route path="/product/:productTitle" element={<Product />} />
 				<Route path="/search/:searchProduct" element={<Search />} />
 				<Route
-					path="/*"
+					path="/NotFound"
 					element={<NotFound message={`Error 404 - Page Not Found!`} />}
 				/>
 				<Route exact path="/checkout" element={<Checkout />} />
-				<Route path="/order*" element={<OrderStatus />} />
+				<Route path="/order/:info" element={<OrderStatus />} />
 			</Routes>
 		</div>
 	);
