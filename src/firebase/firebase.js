c8beeb9873a5e48fb5389fa25b317c14036f8911
hzhec93 <chzhee@gmail.com>
2023-08-12 12:08:16 +0800

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
-// import firebaseConfig from "./firebaseConfig";
+import firebaseConfig from "./firebaseConfig";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -9,7 +9,7 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.1.0/firebase
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 // Initialize Firebase
-const app = initializeApp(import.meta.env.firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
 // Initialize database
 const db = getFirestore(app);
---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,21 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyDtYh-aucrGwgEJH1bEpDfzvTh98Y9hr_M",
+	authDomain: "justdoughit-92876.firebaseapp.com",
+	projectId: "justdoughit-92876",
+	storageBucket: "justdoughit-92876.appspot.com",
+	messagingSenderId: "225964984267",
+	appId: "1:225964984267:web:e77e792618e4db485d984e",
+	measurementId: "G-44WQRTLR52",
+};
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBrltAdJLZPkG6axDkr8rlZVUL-wefLDPQ",
+//   authDomain: "justdoughit-e02d9.firebaseapp.com",
+//   projectId: "justdoughit-e02d9",
+//   storageBucket: "justdoughit-e02d9.appspot.com",
+//   messagingSenderId: "745237535266",
+//   appId: "1:745237535266:web:085254142f58ca3fdb087b",
+//   measurementId: "G-LH5G9W93VX"
+// };
+
+export default firebaseConfig;
